e9e13f328777aad3da5440643532208ec9021e6d
Christian Diener <ch.diener@gmail.com>
2018-09-20 16:16:13 -0700

    also save phyloseq object

--- align.R
+++ align.R
@@ -16,6 +16,16 @@ ref <- readFasta("silva_species.fa.gz")
 ids <- as.character(id(ref))
 annotations <- as.data.table(str_split_fixed(ids, " ", 4))
 names(annotations) <- c("seqnames", "genus", "species", "strain")
-counts_ann <- counts[annotations, on="seqnames"]
+counts_ann <- annotations[counts, on="seqnames"]
 
 fwrite(counts_ann, counts.csv)
+
+mat <- dcast(counts, seqnames ~ sample, value.var="counts", fill=0)
+seqnames <- mat$seqnames
+mat[, seqnames := NULL]
+mat <- as.matrix(mat)
+rownames(mat) <- seqnames
+setkey(annotations, seqnames)
+taxa <- annotations[rownames(mat)]
+ps <- phyloseq(otu_table(mat, taxa_are_rows=TRUE), tax_table(taxa))
+saveRDS(ps, "taxonomy.rds")
